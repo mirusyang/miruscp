53d96a797296ca70682f0ca114aa12b41c735812
Yangjian <yacjyor@gmail.com>
2016-04-06 23:46:04 +0800

    simplified making it at bottom;

--- examples/xkey2/xkey2.cpp
+++ examples/xkey2/xkey2.cpp
@@ -7,9 +7,6 @@
 
   \author 
 
-  \date 
-  2016/04/04
-
   \remarks  
 
 */
@@ -52,133 +49,57 @@ bool XKeyApp::OnInit() {
 
   // So the wxwidgets lib does the delete job... It's may not be a good idea!
   wxImage::AddHandler(new wxPNGHandler());
-/*
-  // the var on stack will make the app crash!
-  wxPNGHandler png_handler;
-  wxImage::AddHandler(&png_handler);
-*/
 
   WarkeyDlg dlg;
   auto retcode = dlg.ShowModal();
   if (wxID_OK == retcode) {
   }
 
-  //wxImage::RemoveHandler(png_handler.GetName());
   return false;
 }
 
 WarkeyDlg::~WarkeyDlg() { }
 
-WarkeyDlg::WarkeyDlg() 
-    // : wxDialog(nullptr, wxID_ANY, "Hello?") { 
-    : wxDialog() {
-  bkgnd_.LoadFile(wxT("bkgnd.bmp"), wxBITMAP_TYPE_BMP);
-  {
-    //wxBitmap win_shape("res/s.png", wxBITMAP_TYPE_PNG);
-    wxBitmap win_shape("bkgnd_mask.bmp", wxBITMAP_TYPE_BMP);
-    long dlgstyle(0);
-    Create(nullptr, wxID_ANY, "Hei", wxDefaultPosition, 
-        wxSize(win_shape.GetWidth(), win_shape.GetHeight()), dlgstyle);
-    wxRegion win_region(win_shape, *wxWHITE);
-    SetWindowStyle(GetWindowStyle() | wxFRAME_SHAPED);
-    SetShape(win_region);
-  }
-  //wxImage img("res/w.png");
-  //wxICON_DEFAULT_TYPE;
-  //wxIcon ico("res/w.ico", wxBITMAP_TYPE_ICO); // wxBITMAP_TYPE_PNG);
-/*
-  wxBitmap bmp("res/w.png", wxBITMAP_TYPE_PNG);
-  wxIcon ico;
-  ico.CopyFromBitmap(bmp);
-  SetIcon(ico);
-*/
-  //SetIcon(wxICON(MAKEINTRESOURCE(IDI_ICON1)));  // not works
-  //SetIcon(wxICON(main_ico));
-  //SetIcon(wxICON(IDI_ICON1)); // not works!!
-/*
-  wxIcon ico("res/w.png", wxBITMAP_TYPE_PNG);
-  SetIcon(ico);
-*/
+WarkeyDlg::WarkeyDlg() : wxDialog() {
+  bkgnd_.LoadFile(wxT("res/bkgnd.bmp"), wxBITMAP_TYPE_BMP);
 
-  {
-    //wxCursor a(wxT("a.cur"), wxBITMAP_TYPE_CUR);
-    wxCursor a(wxT("b.ani"), wxBITMAP_TYPE_ANI);
-    SetCursor(a);
-  }
+  wxBitmap win_shape("res/bkgnd_mask.bmp", wxBITMAP_TYPE_BMP);
+  long dlgstyle(0);
+  Create(nullptr, wxID_ANY, "Hei", wxDefaultPosition, 
+      wxSize(win_shape.GetWidth(), win_shape.GetHeight()), dlgstyle);
+  wxRegion win_region(win_shape, *wxWHITE);
+  SetWindowStyle(GetWindowStyle() | wxFRAME_SHAPED);
+  SetShape(win_region);
 
-/*
-  // This button would carray the hole area of the window!
-  {
-    auto pos = wxDefaultPosition;
-    auto sz = wxDefaultSize;
-    auto okay_btn = new wxButton(this, ID_EXIT, wxT("E&xit"), pos, sz);
-  }
-  {
-    new wxButton(this, ID_EXIT, wxT("E&xit"), wxPoint(150, 200));
-  }
-*/
+  wxCursor cursor(wxT("res/b.ani"), wxBITMAP_TYPE_ANI);
+  SetCursor(cursor);
 
-  { // create widgets on the panel
-    auto panel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 
-        wxTRANSPARENT_WINDOW | wxTAB_TRAVERSAL | wxNO_BORDER);
+  // create widgets on the panel
 #if 0
-    auto vertbox = new wxBoxSizer(wxVERTICAL);
-    auto horzbox1 = new wxBoxSizer(wxHORIZONTAL);
-    auto horzbox2 = new wxBoxSizer(wxVERTICAL);
-    horzbox1->Add(new wxPanel(panel, wxID_ANY));
-    vertbox->Add(horzbox1, wxSizerFlags(1).Expand());
-    vertbox->Add(horzbox2, wxSizerFlags(0).Align(wxALIGN_RIGHT).Border(wxALL, 8));
-    panel->SetSizer(vertbox);
-    auto exit_btn = new wxButton(panel, ID_EXIT, wxT("E&xit"));
-    exit_btn->SetFocus();
-    horzbox2->Add(exit_btn);
+  auto panel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 
+      wxTRANSPARENT_WINDOW | wxTAB_TRAVERSAL | wxNO_BORDER);
+  auto sbox2 = new wxBoxSizer(wxHORIZONTAL);
+  auto sbox = new wxBoxSizer(wxVERTICAL);
+  sbox2->Add(new wxPanel(panel), wxSizerFlags(1).Expand());
+  sbox2->Add(sbox, wxSizerFlags(1).Bottom());
+  panel->SetSizer(sbox2);
+  sbox->Add(new wxButton(panel, ID_EXIT, wxT("E&xit")), 
+      wxSizerFlags(1).Right().Border(wxALL, 8));
+  sbox2->Add(new wxStaticText(panel, wxID_ANY, wxT("Warcraft III kbd&&mouse hook")), 
+      wxSizerFlags(1).Bottom());
 #endif
-    //horzbox1->Add(new wxButton(panel, wxID_OK, wxT("Ok")),
-    //    wxSizerFlags(1).Center());
-    //horzbox1->Add(new wxButton(panel, wxID_CANCEL, wxT("Cancel")), 
-    //    wxSizerFlags(0).Center());
-    //horzbox1->Add(new wxButton(panel, wxID_YES, wxT("Yes")), 
-    //    wxSizerFlags(1).Top().Border(wxTOP, 64));
-    //horzbox1->Add(new wxButton(panel, wxID_NO, wxT("No")), 
-    //    wxSizerFlags(1).Bottom());
-    auto sbox2 = new wxBoxSizer(wxHORIZONTAL);
-    auto sbox = new wxBoxSizer(wxVERTICAL);
-    sbox2->Add(new wxPanel(), wxSizerFlags(1).Expand());
-    sbox2->Add(sbox, wxSizerFlags(0).Bottom());
-    panel->SetSizer(sbox2);
-    sbox->Add(new wxButton(panel, ID_EXIT, wxT("E&xit")), 
-        wxSizerFlags(1).Right().Border(wxALL, 8));
-  }
-
-/*
-  {
-    wxBoxSizer *topsizer = new wxBoxSizer( wxVERTICAL );    
-    // create text ctrl with minimal size 100x60 that is horizontally and    
-    // vertically stretchable with a border width of 10    
-    topsizer->Add(        new wxTextCtrl( this, -1, "My text.", wxDefaultPosition, wxSize(100,60), wxTE_MULTILINE), 
-        wxSizerFlags(1).Align(wxALIGN_TOP).Expand().Border(wxALL, 10));    
-    wxBoxSizer *button_sizer = new wxBoxSizer( wxHORIZONTAL );    
-    //create two buttons that are horizontally unstretchable,    
-    // with an all-around border with a width of 10 and implicit top alignment    
-    button_sizer->Add(        new wxButton( this, wxID_OK, "OK" ),        
-        wxSizerFlags(0).Align(wxALIGN_CENTER).Border(wxALL, 10));    
-    button_sizer->Add(        new wxButton( this, wxID_CANCEL, "Cancel" ),        
-        wxSizerFlags(0).Align(wxALIGN_CENTER).Border(wxALL, 10));    
-    //create a sizer with no border and centered horizontally    
-    topsizer->Add(        button_sizer,        wxSizerFlags(0).Center() );    
-    SetSizerAndFit(topsizer); // use the sizer for layout and set size and hints
-  }
-*/
-
-/*
-  // doesn't work....
-  { // another way to creates widgets...
-    auto btn_sizer = new wxBoxSizer(wxVERTICAL);
-    btn_sizer->Add(new wxButton(this, ID_EXIT, wxT("E&xit")), 
-        wxSizerFlags(0).Center().Border(wxALL, 8));
-    SetSizerAndFit(btn_sizer);
-  }
-*/
+  //auto top_panel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 
+  //    wxTRANSPARENT_WINDOW | wxTAB_TRAVERSAL | wxNO_BORDER);
+  //auto bottom_panel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 
+  //    wxTRANSPARENT_WINDOW | wxTAB_TRAVERSAL | wxNO_BORDER);
+  auto bs = new wxBoxSizer(wxHORIZONTAL);
+  auto bs2 = new wxBoxSizer(wxVERTICAL);
+  SetSizer(bs);
+  bs->Add(bs2, wxSizerFlags(1).Bottom());
+  //bs->Add(top_panel, wxSizerFlags(1));
+  //bs->Add(bottom_panel, wxSizerFlags(1));
+  bs2->Add(new wxButton(this, ID_EXIT, wxT("E&xit")), 
+      wxSizerFlags(0).Right().Border(wxALL, 8));
 
   Centre();
 }
